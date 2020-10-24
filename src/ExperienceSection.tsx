e690b543430b8305ca2103f4d87f840c5407a13a
Joe Gaebel <joe.gaebel@hotmail.com>
2020-10-24 21:33:32 +1100

    Flesh out the rest of Pivotal

--- src/ExperienceSection.tsx
+++ src/ExperienceSection.tsx
@@ -24,14 +24,15 @@ const ExperienceSection = () => {
                 name="Mobile App team at Service NSW"
                 projectName="COVID Safe Check-in"
                 image={service}
-                description="As a part of this project, I helped deliver the venue check-ins for COVID contact tracing feature within the Service NSW Mobile App"
-                keywords={["React Native", "Detox", "Node.js", "Firebase", "Jest", "ES6+"]}
+                description="As a part of this project, Helped deliver the venue check-ins for COVID contact tracing feature within the Service NSW Mobile App"
+                keywords={["React Native", "Redux", "Detox", "Node.js", "Firebase", "Jest", "ES6+"]}
             />
             <Client
                 name="RMS Transactions team at Service NSW"
+                link="https://www.service.nsw.gov.au/transaction/renew-vehicle-registration"
                 projectName="Renew Registration"
                 image={service}
-                description="I helped rewrite from-scratch the legacy Renew Registration transaction into a Test Driven and easily maintainable Spring Boot application"
+                description="Helped rewrite from-scratch the legacy Renew Registration transaction into a Test Driven and easily maintainable Spring Boot application"
                 keywords={["Spring Boot", "Kotlin", "React", "Apigee", "TestCafe", "Jest", "MockK", "Junit 5"]}
             />
             <Client
@@ -39,7 +40,7 @@ const ExperienceSection = () => {
                 projectName="COVID-19 Test Result Notifications"
                 image={service}
                 description="I lead the mobile app team in producing a NodeJS microservice responsible for registering devices and sending push notifications through Firebase to deliver COVID-19 test results"
-                keywords={["React Native", "Detox", "Node.js", "Firebase", "Jest", "ES6+"]}
+                keywords={["React Native", "Redux", "Detox", "Node.js", "Firebase", "Jest", "ES6+"]}
             />
         </PivotalLabs>
 
@@ -48,54 +49,62 @@ const ExperienceSection = () => {
                 name="Payments team at Service NSW"
                 projectName="Government Payments Platform"
                 image={service}
-                description="I helped the Payments team build the NSW Payment System, responsible for processing millions of dollars daily"
-                keywords={["Spring Boot", "Kotlin", "Vue", "RabbitMQ", "Postgres", "Apigee", "Cypress", "Jest"]}
+                description="Helped the Payments team build the NSW Government Payment System, responsible for processing millions of dollars daily"
+                keywords={["Spring Boot", "Kotlin", "Vue", "VueX", "RabbitMQ", "Postgres", "Apigee", "Cypress", "Jest"]}
             />
             <Client
                 name="Service For Business at Service NSW"
                 projectName="Contractor Licence"
                 image={service}
+                link="https://mybusiness.service.nsw.gov.au/transactions/electrical-individual-contractor-licence/ready-to-start"
                 description="I lead the transformation of the time-consuming contractor licence application form into a digital experience. This application form receives the data, and forwards it to the agency directly, saving hours of time for Tradespeople and back office staff"
-                keywords={["Spring Boot", "Kotlin", "React", "Postgres", "Apigee"]}
+                keywords={["Spring Boot", "Kotlin", "React", "Redux", "Postgres", "Apigee"]}
             />
             <Client
                 name="Vouchers and Rebates team at Service NSW"
                 projectName="Active Kids and Creative Kids vouchers"
                 image={service}
-                description="I helped the Vouchers and Rebates team create the Active Kids/Creative Kids voucher application from scratch to replace the legacy application"
-                keywords={["Spring Boot", "Kotlin", "Vue", "Postgres", "Apigee"]}
+                link="https://www.service.nsw.gov.au/campaign/active-kids"
+                description="Helped the Vouchers and Rebates team create the Active Kids/Creative Kids voucher website and backend system from scratch to replace the legacy application"
+                keywords={["Spring Boot", "Kotlin", "Vue", "VueX", "Postgres", "Apigee"]}
             />
             <Client
                 name="Tell Government Once team at Service NSW"
-                projectName="Active Kids and Creative Kids vouchers"
+                projectName="Notification API"
                 image={service}
-                description="I helped Service NSW build the ability to recieve COVID test results directly to their phone"
+                description="Helped the TGO team create a microservice responsible for maintaining citizens most recent contact information and contact preferences. If you've gotten an email notification from Service NSW, it's gone through this API"
+                keywords={["Spring Boot", "Kotlin", "React", "Postgres", "Apigee"]}
             />
             <Client
-                name="Easy to Do Business team at Service NSW"
+                name="Service For Business at Service NSW"
                 projectName="Cafe, Restaurant and Small Business application"
                 image={service}
-                description="I helped Service NSW build the ability to recieve COVID test results directly to their phone"
+                link="https://mybusiness.service.nsw.gov.au/dashboard"
+                description="Helped Service NSW digitize forms for opening a Cafe/Restaurant/Small Business"
+                keywords={["Spring Boot", "Kotlin", "React", "Redux", "Postgres", "Apigee"]}
             />
             <Client
                 name="CoreLogic"
                 projectName="ValConnect"
                 image={corelogic}
-                description="I helped Service NSW build the ability to recieve COVID test results directly to their phone"
+                description="Helped CoreLogic build and deliver a mobile app for quickly performing property assessments"
+                keywords={["React Native", "Redux", "Java", "Spring"]}
             />
             <Client
                 name="Telstra"
-                projectName="TESA"
+                projectName="Telstra Essential Sales Assistant"
                 image={telstra}
                 imageSizeOverride="35px"
-                description="I helped Service NSW build the ability to recieve COVID test results directly to their phone"
+                description="Built an internal website to be used by the sales team to be a one-stop-shop for supporting enterprise sales"
+                keywords={["Kotlin", "Spring Boot", "React", "Redux", "Semantic UI"]}
             />
             <Client
                 name="Telstra"
                 projectName="Prepaid Recharge"
                 imageSizeOverride="35px"
                 image={telstra}
-                description="I helped Service NSW build the ability to recieve COVID test results directly to their phone"
+                link="https://www.telstra.com.au/mobile-phones/prepaid-mobiles/prepaid-recharge"
+                description="Built the system to collect payments for Telstra's prepaid service customers"
             />
         </PivotalLabs>
     </Section>
@@ -142,7 +151,7 @@ const PivotalLabs: FunctionComponent<PivotalProps> = ({children, title, dateStri
         </div>
 
         <div className="mb-5">
-            <div className="subtitle body-font is-size-6 header-font">Teams I've worked with:</div>
+            <div className="subtitle body-font is-size-6 header-font">Products I've helped build:</div>
         </div>
 
         <div className="columns is-multiline">
@@ -159,22 +168,27 @@ interface ClientProps {
     description: string
     imageSizeOverride?: string
     keywords?: string[]
+    link?: string
 }
 
-const Client: FunctionComponent<ClientProps> = ({name, image, description, projectName, imageSizeOverride, keywords}) => {
+const Client: FunctionComponent<ClientProps> = ({name, image, description, projectName, imageSizeOverride, keywords, link}) => {
     return <div className="column is-one-third mb-0">
         <div className="card">
             <div className="card-content">
-                <div className="mb-5" style={{display: "flex", justifyContent: "space-between"}}>
+                <div className="mb-2" style={{display: "flex", justifyContent: "space-between"}}>
                     <div>
                         <div className="header-font is-size-6 subtitle mb-1">{projectName}</div>
-                        <div className="header-font is-size-7 subtitle">{name}</div>
+                        <div className="header-font is-size-7 subtitle mb-0">{name}</div>
                     </div>
                     <div className="pl-2">
                         <img width={imageSizeOverride || "80px"} height="auto" src={image}/>
                     </div>
 
                 </div>
+
+                {!link && <div className="mt-4"/>}
+
+                {link && <div className="mb-2 body-font"><a className="is-size-7" href={link}>{link}</a></div>}
                 <div>{description}</div>
 
                 {keywords && <div className="mt-4">
