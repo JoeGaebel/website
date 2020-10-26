12864e333afe5d32b0f53638579d0b77a2f45503
Joe Gaebel <joe.gaebel@hotmail.com>
2020-10-26 22:54:11 +1100

    Defunk the nav bar

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,24 +1,22 @@
 import React from "react"
-import sailboat from "./icons/sailboat.svg"
 
-const Navigation = () => <nav className="navbar is-transparent is-fixed-top">
-    <div className="navbar-brand">
-        <img src={sailboat} className="ml-2" width="45px"/>
-    </div>
-    <div id="navbarExampleTransparentExample" className="navbar-menu">
-        <div className="navbar-start">
-            <a className="navbar-item" href="#experience">
-                Experience
-            </a>
-            <a className="navbar-item" href="#projects">
-                Projects
-            </a>
-            <a className="navbar-item" href="#education">
-                Education
-            </a>
-            <a className="navbar-item" href="#contact">
-                Contact me
-            </a>
+const Navigation = () => <nav className="navbar is-transparent is-fixed-top is-flex is-justify-content-center is-align-items-center is-hidden-mobile">
+    <div>
+        <div className="navbar-menu">
+            <div className="navbar-start">
+                <a className="navbar-item" href="#experience">
+                    Experience
+                </a>
+                <a className="navbar-item" href="#projects">
+                    Projects
+                </a>
+                <a className="navbar-item" href="#education">
+                    Education
+                </a>
+                <a className="navbar-item" href="#contact">
+                    Contact me
+                </a>
+            </div>
         </div>
     </div>
 </nav>
