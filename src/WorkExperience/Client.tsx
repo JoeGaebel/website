e620b3493d4f7e81edf8b5b338eedfe50af147ed
Joe Gaebel <joe.gaebel@hotmail.com>
2020-10-26 22:24:12 +1100

    Add client hiding

--- src/WorkExperience/Client.tsx
+++ src/WorkExperience/Client.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useContext} from "react";
+import React, {CSSProperties, FunctionComponent, useContext, useRef} from "react";
 import {AppState, StateContext} from "../App";
 
 interface ClientProps {
@@ -15,8 +15,62 @@ interface ClientListProps {
     experienceName?: string
 }
 
+const overflowHiddenStyle: CSSProperties = {
+    maxHeight: "40rem",
+    position: "relative",
+    overflow: "hidden"
+}
+
+const seeMoreStyle: CSSProperties = {
+    position: "absolute",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    bottom: "0",
+    left: "0",
+    width: "100%",
+    textAlign: "center",
+    height: "40%",
+    backgroundImage: "linear-gradient(to bottom, transparent, white)"
+}
+
+const seeLessStyle: CSSProperties = {
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+}
+
+interface HidingWrapperProps {
+    showingProjects: boolean | undefined
+}
+const HidingWrapper: FunctionComponent<HidingWrapperProps> = ({children, showingProjects}) => {
+    if (!showingProjects) {
+        return <div style={overflowHiddenStyle}>{children}</div>
+    } else {
+        return <div>{children}</div>
+    }
+}
+
+
+interface HidingButtonProps {
+    showingProjects: boolean | undefined
+    onClick: () => void
+}
+const HidingButton: FunctionComponent<HidingButtonProps> = ({showingProjects, onClick}) => {
+    if (!showingProjects) {
+        return <div style={seeMoreStyle}>
+            <button className="button is-medium is-info" onClick={onClick}>See more</button>
+        </div>
+    } else {
+        return <div style={seeLessStyle}>
+            <button className="mt-4 button is-medium is-info" onClick={onClick}>See less</button>
+        </div>
+    }
+}
+
 export const ClientList: FunctionComponent<ClientListProps> = ({children, experienceName = ""}) => {
     const [state, setState] = useContext(StateContext);
+    const ref = useRef<HTMLDivElement>(null)
 
     function handleClick() {
         setState!((oldState: AppState) => {
@@ -25,10 +79,14 @@ export const ClientList: FunctionComponent<ClientListProps> = ({children, experi
                 [experienceName]: !oldState[experienceName]
             }
         })
+
+        if (state?.[experienceName]) {
+            window.scrollTo(0, ref.current!.offsetTop)
+        }
     }
 
-    if (state?.[experienceName]) {
-        return <>
+    return <div ref={ref}>
+        <HidingWrapper showingProjects={state?.[experienceName]}>
             <div className="mb-5 mt-6">
                 <div className="header-font title is-size-4 mb-4">Products I've helped build:</div>
             </div>
@@ -37,16 +95,9 @@ export const ClientList: FunctionComponent<ClientListProps> = ({children, experi
                 {children}
             </div>
 
-            <button className="button is-medium is-info is-light" style={{width: "100%"}} onClick={handleClick}>Hide portfolio</button>
-        </>
-    }
-
-
-    return <>
-        <div className="mb-5 mt-6">
-            <button className="button is-medium is-info is-light" style={{width: "100%"}} onClick={handleClick}>Show portfolio...</button>
-        </div>
-    </>
+            <HidingButton showingProjects={state?.[experienceName]} onClick={handleClick}/>
+        </HidingWrapper>
+    </div>
 }
 
 export const Client: FunctionComponent<ClientProps> = ({name, image, description, projectName, imageSizeOverride, keywords, link}) => {
@@ -67,7 +118,8 @@ export const Client: FunctionComponent<ClientProps> = ({name, image, description
 
                 {!link && <div className="mt-4"/>}
 
-                {link && <div className="mb-2 body-font" style={{lineHeight: "16px"}}><a target="_blank" className="is-size-7" href={link}>{link}</a></div>}
+                {link && <div className="mb-2 body-font" style={{lineHeight: "16px"}}>
+                    <a target="_blank" className="is-size-7" href={link}>{link}</a></div>}
                 <div>{description}</div>
 
                 {keywords && <div className="mt-4">
