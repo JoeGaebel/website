dbb86961226836db871f31a719f7267ba31deadd
Joe Gaebel <joe.gaebel@hotmail.com>
2020-11-03 23:30:55 +1100

    Fix the thousand interval problem

--- src/HeaderSection.tsx
+++ src/HeaderSection.tsx
@@ -7,7 +7,7 @@ const fadeSlideIn = merge(fadeIn, slideInLeft)
 const slideInAnimation = keyframes`${fadeSlideIn}`;
 
 const SlideInDiv = styled.div`
-  animation: 2s ${slideInAnimation};
+  animation: 1s ${slideInAnimation};
 `;
 
 const HeaderSection = () => {
@@ -27,17 +27,16 @@ const SlidingList = () => {
         "Fitness Geek",
         "Meditator",
         "Motorcycle Enthusiast",
-        "Kombucha lover",
+        "Kombucha lover"
     ]
 
     const [index, setIndex] = useState<number>(0)
 
     useEffect(() => {
         setInterval(() => {
-            console.log()
-            setIndex((index + 1) % proofs.length)
+            setIndex(i => (i + 1) % proofs.length)
         }, 4000)
-    })
+    }, [setIndex, proofs.length])
 
     return <SlideInDiv key={index}>{proofs[index]}</SlideInDiv>
 }
