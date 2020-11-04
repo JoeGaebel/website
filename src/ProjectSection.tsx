791ee22e7698da6d3491904d15ff4ac91bfc50e8
pivotal <pivotal@pivotal.io>
2020-11-04 23:36:01 +1100

    Style projects a bit more

--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -6,6 +6,7 @@ import spherelink from "./projects/spherelink.png"
 import linkIcon from "./icons/link.svg"
 import githubIcon from "./icons/github.png"
 import dylAndJoe from "./projects/dylAndJoe.jpg"
+import styled from "styled-components";
 
 const SneakyTag: FunctionComponent = ({children}) => {
     return <span style={{fontSize: "0.5rem"}} className="tag is-info is-light mr-1 mt-1">{children}</span>
@@ -21,15 +22,16 @@ const ProjectSection = () => {
             <DylAndJoeGoToSanFrancisco/>
         </ProjectsList>
         <div className="is-flex is-justify-content-flex-end">
-            <a className="has-text-grey has-text-weight-light is-size-7" href="https://www.github.com/joegaebel/website">
-                joegaebel.com is also a project <SneakyTag>TypeScript</SneakyTag><SneakyTag>React</SneakyTag><SneakyTag>Bulma</SneakyTag></a>
+            <a className="has-text-grey has-text-weight-light is-size-7"
+               href="https://www.github.com/joegaebel/website">
+                joegaebel.com is also a
+                project <SneakyTag>TypeScript</SneakyTag><SneakyTag>React</SneakyTag><SneakyTag>Bulma</SneakyTag></a>
         </div>
     </Section>
 }
 
-
 const ProjectsList: FunctionComponent = ({children}) => {
-    return <div className="is-flex-direction-column">
+    return <div className="columns is-multiline">
         {children}
     </div>
 }
@@ -45,35 +47,34 @@ interface ProjectProps {
 
 const Project: FunctionComponent<ProjectProps> = (props) => {
     const {name, image, description, githubLink, projectLink, keywords} = props;
-    return <div className="card mb-4 px-6 py-4">
-        <div className="cardContent">
-            <div className="subtitle body-font is-size-4 mb-4">{name}</div>
-            <div className="columns">
-                <div className="column is-two-fifths mr-2">
+    return <div className="column is-half">
+        <div className="card my-3">
+        <div className="card-content">
+            <div className="card-image mb-5">
+                <figure className="image">
                     <a target="_blank" href={projectLink}><img src={image}/></a>
+                </figure>
+            </div>
+            <div className="subtitle header-font has-text-weight-semibold is-size-4">{name}</div>
+            {description}
+            <div className="block is-flex">
+                <div className="subtitle body-font is-size-7 mb-2">
+                    <a target="_blank" href={projectLink}><img width="30px" src={linkIcon}/></a>
                 </div>
-                <div className="column">
-                    {description}
-                    <div className="block is-flex">
-                        <div className="subtitle body-font is-size-7 mb-2">
-                            <a target="_blank" href={projectLink}><img width="30px" src={linkIcon}/></a>
-                        </div>
-                        <div className="subtitle body-font is-size-7 mb-2">
-                            <a target="_blank" href={githubLink}><img width="30px" src={githubIcon}/></a>
-                        </div>
-                    </div>
-                    <div className="block is-relative">
-                        <div>Built with:</div>
-                        <div>
-                            {keywords.map((keyword, index) =>
-                                <span key={index}
-                                      className="tag is-info is-light mr-1 mt-1 is-size-7">{keyword}</span>)}
-                        </div>
-                    </div>
+                <div className="subtitle body-font is-size-7 mb-2">
+                    <a target="_blank" href={githubLink}><img width="30px" src={githubIcon}/></a>
+                </div>
+            </div>
+            <div className="block is-relative">
+                <div>Built with:</div>
+                <div>
+                    {keywords.map((keyword, index) =>
+                        <span key={index}
+                              className="tag is-info is-light mr-1 mt-1 is-size-7">{keyword}</span>)}
                 </div>
             </div>
         </div>
-    </div>
+        </div></div>
 }
 
 
@@ -97,7 +98,8 @@ const Rentifier = () => <Project
     description={<>
         <div className="block">Create a rental property pipeline and filter thousands down to your favourite one.</div>
         <div className="block">
-            Using TestCafe to scrape rental property websites and google distance APIs, this project spits out a CSV, incorporating travel times to your favourite places.
+            Using TestCafe to scrape rental property websites and google distance APIs, this project spits out a CSV,
+            incorporating travel times to your favourite places.
             This allows you to import the CSV into Google Sheets, immediately simplifying your property hunting.
         </div>
     </>}
@@ -112,7 +114,8 @@ const Spherelink = () => <Project
     description={<>
         <div className="block">Immortalize your most cherished memories.</div>
         <div className="block">
-            Upload and link together 360 photos to recreate your memories. Tag the most important areas, embed content, make it fully immersive with sound and music, and fully preserve the memories that matter most.
+            Upload and link together 360 photos to recreate your memories. Tag the most important areas, embed content,
+            make it fully immersive with sound and music, and fully preserve the memories that matter most.
         </div>
         <div className="block">I bought and lived on a sail boat so I could afford the rent in SF, check it out!</div>
     </>}
@@ -127,11 +130,13 @@ const DylAndJoeGoToSanFrancisco = () => <Project
     description={<>
         <div className="block">Went on a do or die journey to get jobs in San Francisco.</div>
         <div className="block">
-            In the span of two weeks, my good friend and I built a quick website, flew out to San Francisco, and knocked on as many company doors as we could.
+            In the span of two weeks, my good friend and I built a quick website, flew out to San Francisco, and knocked
+            on as many company doors as we could.
         </div>
 
         <div className="block">
-            Afterwards we spend the next few months avoiding school work and doing interviews. In the end, we both got to work for San Frantastic companies.
+            Afterwards we spend the next few months avoiding school work and doing interviews. In the end, we both got
+            to work for San Frantastic companies.
         </div>
     </>}
     githubLink="https://github.com/JoeGaebel/dylandjoegotosanfrancisco"
