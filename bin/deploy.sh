1a314f6864122155ba9fc13b5563cef215c607c1
Joe Gaebel <joe.gaebel@hotmail.com>
2021-03-08 15:06:08 +1100

    weird non-deploy error

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ rm -rf ./joegaebel.github.io/!(CNAME)
 cp -r build/* ./joegaebel.github.io
 cd joegaebel.github.io
 git add .
-git commit -m "Update site"
+git commit -m "Update site" --allow-empty
 git push origin master
 cd ..
 git submodule update --remote
